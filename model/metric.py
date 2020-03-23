57fb9da9923f0b06c36c3a7783d3b543aedf6d0f
Ahad Chaudhry <ahadc@sfu.ca>
2020-03-22 21:45:18 -0700

    Fix IoU metric

--- model/metric.py
+++ model/metric.py
@@ -1,39 +1,28 @@
 import torch
 
 
-def accuracy(output, target):
-    with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
+def _preprocess(output, target):
+    output = output.argmax(dim=1)
+    output = output.to(torch.uint8)
+    # output = output.squeeze(dim=1)  # BATCH x 1 x H x W => BATCH x H x W
 
-
-def top_k_acc(output, target, k=3):
-    with torch.no_grad():
-        pred = torch.topk(output, k, dim=1)[1]
-        assert pred.shape[0] == len(target)
-        correct = 0
-        for i in range(k):
-            correct += torch.sum(pred[:, i] == target).item()
-    return correct / len(target)
+    target = (target * 255).squeeze(dim=1)
+    target = target.to(torch.uint8)
+    target[target == 255] = 0
+    return output, target
 
 
 def meanIoU(output, target):
     SMOOTH = 1e-6
-    output = output.argmax(dim=1)
-    output = output.to(torch.uint8)
-
-    target = (target * 255).squeeze(dim=1)
-    target = target.to(torch.uint8)
-
-
-    output = output.squeeze(dim=1)  # BATCH x 1 x H x W => BATCH x H x W
     
+    output, target = _preprocess(output, target)
     intersection = (output & target).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
     union = (output | target).float().sum((1, 2))         # Will be zero if both are 0
     
-
     iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
-    return iou.mean().item()
+
+    # This is equal to comparing with thresolds
+    # The minimum value of a correct IoU is 0.5, the following will measure how correct a corect
+    # segmentation is (segmentation with IoU > 0.5).
+    # thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10
+    return iou.mean()
