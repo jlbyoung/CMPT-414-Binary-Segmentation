cd5b4917b09964393115b40c14ec40a5d21aa1ae
zapsel <julianjo2@gmail.com>
2020-04-03 23:42:15 -0700

    Fixed albumentations tensor issues
    Needs further experimentation to get better loss and miou

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -5,67 +5,164 @@ import numpy as np
 import random
 import torch
 
-def transform_pipeline(image, mask):
+from torch.utils.data import Dataset
+from PIL import Image
+import cv2
+from torchvision import transforms
+from albumentations.pytorch import (
+    ToTensor,
+    ToTensorV2
+)
+
+from albumentations import (
+    PadIfNeeded,
+    IAAAdditiveGaussianNoise,
+    IAASharpen,
+    IAAEmboss,
+    IAAPiecewiseAffine,
+    GaussNoise,
+    MedianBlur,
+    MotionBlur,
+    Blur,
+    HorizontalFlip,
+    VerticalFlip,    
+    CenterCrop,    
+    Crop,
+    Compose,
+    Transpose,
+    RandomRotate90,
+    Normalize,
+    Resize,
+    ElasticTransform,
+    GridDistortion, 
+    OpticalDistortion,
+    RandomSizedCrop,
+    OneOf,
+    CLAHE,
+    RandomBrightnessContrast,
+    HueSaturationValue,  
+    RandomGamma,
+    MultiplicativeNoise,
+    ChannelDropout,
+    ChannelShuffle    
+)
+
+albumentations_transform = Compose([
+        
     # Note resize and crop first if scaling down to make the
     # other transformations more efficient
-
-    # Resize 
-    resize = transforms.Resize(size=(256, 256))
-    image = resize(image)
-    mask = resize(mask)
+    Resize(256, 256),
 
     # Random crop
-    i, j, h, w = transforms.RandomCrop.get_params(image, output_size=(224, 224))
-    image = TF.crop(image, i, j, h, w)
-    mask = TF.crop(mask, i, j, h, w)
+    RandomSizedCrop(
+        p=1,
+        min_max_height=(224, 224),
+        height=256,
+        width=256
+    ),
 
-    # Apply one by using elif for subsequent if statements
+    #TODO: Implement ShiftScaleRotate
 
-    # Randomly change brightness
-    if random.random() > 0.5:
-        # Brightness factor with a range of (0.5, 1.5)
-        image = TF.adjust_brightness(image, random.random() + 0.5)
+    # Image Flips
+    OneOf([
+        VerticalFlip(p=0.3),
+        HorizontalFlip(p=0.3),
+        RandomRotate90(p=0.5),
+        Transpose(p=0.4),
+        ], p=0.5
+    ),
 
-    # Randomly change contrast
-    if random.random() > 0.5:
-        # Contrast factor with a range of (0.5, 2.0)
-        image = TF.adjust_contrast(image, random.randint(5, 20) / 10)
+    # Blurring, causing isues with tensor conversion
+    OneOf([
+       MotionBlur(p=0.2),
+       MedianBlur(blur_limit=3, p=0.4),
+       Blur(blur_limit=3, p=0.2),
+       ], p=0.2
+    ),
 
-    # Randomly change gamma
-    if random.random() > 0.5:
-        # Contrast factor with a range of (0.5, 1.5)
-        image = TF.adjust_gamma(image, random.random() + 0.5)
+    # Image Warping/ Distortion
+    OneOf([
+        ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+        OpticalDistortion(p=0.3),
+        GridDistortion(p=0.1),
+        #IAAPiecewiseAffine(p=0.3),
+        ], p=0.2
+    ),
 
-    # Randomly change hue
-    if random.random() > 0.5:
-        # Contrast factor with a range of (-0.5, 0.5)
-        image = TF.adjust_hue(image, random.random() - 0.5)
+    #Add varying levels and types of noise
+    OneOf([
+        IAAAdditiveGaussianNoise(p=0.4),
+        GaussNoise(p=0.4),
+        MultiplicativeNoise(multiplier=0.5, p=0.2),
+        MultiplicativeNoise(multiplier=1.5, p=0.1),
+        MultiplicativeNoise(multiplier=[0.5, 1.5], per_channel=True, p=0.2),
+        MultiplicativeNoise(multiplier=[0.5, 1.5], elementwise=True, p=0.2),
+        MultiplicativeNoise(multiplier=[0.5, 1.5], elementwise=True, per_channel=True, p=0.2),
+        ],p=0.3
+    ),
 
-    # Random horizontal flipping
-    if random.random() > 0.5:
-        image = TF.hflip(image)
-        mask = TF.hflip(mask)
 
-    # Random vertical flipping
-    if random.random() > 0.5:
-        image = TF.vflip(image)
-        mask = TF.vflip(mask)
+    # Randomly Change Brightness/Sharpness/Embossment
+    OneOf([
+        CLAHE(clip_limit=2),
+        IAASharpen(),
+        IAAEmboss(),
+        RandomBrightnessContrast(),
+        ], p=0.4
+    ),
 
-    # Random rotation between -10 and 10 degrees
-    if random.random() > 0.5:
-        angle = random.randint(-10, 10)
-        image = TF.rotate(image, angle)
-        mask = TF.rotate(mask, angle)
+    # Change Gamma and Saturation
+    HueSaturationValue(p=0.3),
+    RandomGamma(p=0.4),
 
-    # Transform to tensor
-    image = TF.to_tensor(image)
-    mask = np.array(mask)
-    mask = torch.from_numpy(mask).to(torch.long)
+    # Random Color Channel manipulation
+    OneOf([
+        ChannelShuffle(p=0.4),
+        ChannelDropout(channel_drop_range=(1, 1), fill_value=0, p=0.2),
+        ChannelDropout(channel_drop_range=(1, 1), fill_value=0, p=0.2),
+        ChannelDropout(channel_drop_range=(1, 1), fill_value=0, p=0.2),
+        ChannelDropout(channel_drop_range=(1, 1), fill_value=0, p=0.2),
+        ChannelDropout(channel_drop_range=(1, 1), fill_value=0, p=0.2),
+        ChannelDropout(channel_drop_range=(2, 2), fill_value=0, p=0.2),
+        ChannelDropout(channel_drop_range=(2, 2), fill_value=0, p=0.2),
+        ChannelDropout(channel_drop_range=(2, 2), fill_value=0, p=0.2),
+        ], p=0.2
+    ),
 
     # Normalize image using values from ImageNet
-    image = TF.normalize(image, 
-                         mean = [0.485, 0.456, 0.406], 
-                         std = [0.229, 0.224, 0.225]) 
+    Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225],
+    ),
+
+    # Convert to Tensor
+    #ToTensor()
+])
+
+
+def transform_pipeline(image, mask):
+
+    # Convert image and mask to numpy arrays
+    image = np.array(image)
+    mask = np.array(mask)
+
+    # Get the composed albumentation transforms
+    # augmentation = albumentations_transform()
+    # data = {"image": image, "mask": mask}
+    # augmented = augmentation(**data)
+
+    aug = Compose([Resize(256,256), RandomSizedCrop(min_max_height=(224, 224),
+            height=256,
+            width=256)])
+
+    augmented = albumentations_transform(image=image,mask=mask)
+
+    # Extract transformed image and mask
+    image = augmented['image']
+    mask = augmented['mask']
+
+    image = TF.to_tensor(image)
+    mask = torch.from_numpy(mask).to(torch.long)
     return image, mask
     
 
