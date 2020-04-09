b7e0d8e413a130ac391250fb24386dba701e91c1
ahadc <ahadc@sfu.ca>
2020-04-09 14:43:25 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,14 +11,14 @@ git config --global user.name ""
 
 git checkout add-binary-classification # Replace with your branch here
 
+chmod +x download_dataset.sh
+./download_dataset.sh &
 
 #install packages
 sudo chmod +rw /opt/conda/pkgs/qt-5.12.5-hd8c4c69_1/info/paths.json
 conda env create -f environment.yml
 conda install --name cv414 pytorch torchvision cudatoolkit=10.1 -c pytorch
 
-chmod +x download_dataset.sh
-./download_dataset.sh
 
 cp imagesets/*.txt data/VOCdevkit/VOC2012/ImageSets/Segmentation
 # We can add all the commands we want to run here
