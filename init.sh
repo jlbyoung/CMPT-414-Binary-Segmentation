45f19ccc1b02f35dcc9d61672b9ca0fb8346ecea
Ahad Chaudhry <ahadc@sfu.ca>
2020-04-09 04:23:47 +0000

    Update init and download dataset scripts

--- init.sh
+++ init.sh
@@ -1,15 +1,24 @@
 # Sometimes the notebook may run into random issues, if that happens just run it again
 
-# Setup repository
+ Setup repository
 git clone https://github.com/jlbyoung/CMPT-414-Rotoscoping.git
 cd CMPT-414-Rotoscoping
+
+
+git config --global user.email ""
+git config --global user.name ""
+
+
 git checkout master # Replace with your branch here
 
+
 #install packages
+sudo chmod +rw /opt/conda/pkgs/qt-5.12.5-hd8c4c69_1/info/paths.json
 conda env create -f environment.yml
 conda activate cv414
 conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
 
-
+chmod +x download_dataset.sh
+./download_dataset.sh
 # We can add all the commands we want to run here
 # python train.py -c collabConfig.json
